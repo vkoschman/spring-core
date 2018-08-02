b387601b165f85b684e291d8cfcb4d61c2543a51
Andrei Dremov <andrei_dremov@epam.com>
2018-08-02 19:54:29 +0300

    Step 5 | Show multiple beans conflict and Qualifier annotation

--- src/main/java/com/andreitop/di/mount/Wolf.java
+++ src/main/java/com/andreitop/di/mount/Wolf.java
@@ -1,5 +1,8 @@
 package com.andreitop.di.mount;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Wolf implements Mount {
     public void move() {
         System.out.println("I'm a wolf!");
--- src/main/java/com/andreitop/di/unit/Human.java
+++ src/main/java/com/andreitop/di/unit/Human.java
@@ -1,6 +1,7 @@
 package com.andreitop.di.unit;
 
 import com.andreitop.di.mount.Mount;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
@@ -9,6 +10,7 @@ import javax.inject.Inject;
 public class Human implements Unit {
 
     @Inject
+    @Qualifier("wolf")
     private Mount mount;
 
     @Override
